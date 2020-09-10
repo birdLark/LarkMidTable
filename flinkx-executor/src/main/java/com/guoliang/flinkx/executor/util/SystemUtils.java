7772374f9f35b6efe3242b0f3a70164e2faf2174
wxgzgl <wxgzgl@gmail.com>
2020-09-10 15:58:37 +0800

    Java调用shell的工具类

--- flinkx-executor/src/main/java/com/guoliang/flinkx/executor/util/SystemUtils.java
+++ flinkx-executor/src/main/java/com/guoliang/flinkx/executor/util/SystemUtils.java
@@ -4,7 +4,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author maokeluo
@@ -37,4 +40,31 @@ public class SystemUtils {
         //LOGGER.info("DATAX_HOME:{}", DATAX_HOME);
         return DATAX_HOME;
     }
+
+    public static  void executeShell(String command) {
+		BufferedReader reader = null;
+		try {
+			command = "sh /home/hadoop/data/install.sh";
+			System.out.println(command);
+			Runtime runtime = Runtime.getRuntime();
+			Process pro = runtime.exec(command);
+			int status = pro.waitFor();
+			if (status != 0) {
+				System.out.println("Failed to call shell's command ");
+			}
+			BufferedReader br = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+			StringBuffer strbr = new StringBuffer();
+			String line;
+			while ((line = br.readLine()) != null) {
+				strbr.append(line).append("\n");
+			}
+			String result = strbr.toString();
+			System.out.println(result);
+		} catch (IOException ec) {
+			ec.printStackTrace();
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+	}
+
 }
