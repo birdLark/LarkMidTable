7c16e661c22a6f376de9f04be8b43720ce34c7c2
wxgzgl <wxgzgl@gmail.com>
2020-09-28 20:32:12 +0800

    update install

--- flinkx-admin/src/main/java/com/guoliang/flinkx/admin/tool/flinkx/reader/MongoDBReader.java
+++ flinkx-admin/src/main/java/com/guoliang/flinkx/admin/tool/flinkx/reader/MongoDBReader.java
@@ -24,17 +24,8 @@ public class MongoDBReader extends BaseReaderPlugin implements FlinkxReaderInter
         Map<String, Object> readerObj = Maps.newLinkedHashMap();
         readerObj.put("name", getName());
         Map<String, Object> parameterObj = Maps.newLinkedHashMap();
-        String[] addressList = null;
-        String str = dataSource.getJdbcUrl().replace(Constants.MONGO_URL_PREFIX, Constants.STRING_BLANK);
-        if (str.contains(Constants.SPLIT_AT) && str.contains(Constants.SPLIT_DIVIDE)) {
-            addressList = str.substring(str.indexOf(Constants.SPLIT_AT) + 1, str.indexOf(Constants.SPLIT_DIVIDE)).split(Constants.SPLIT_COMMA);
-        } else if (str.contains(Constants.SPLIT_DIVIDE)) {
-            addressList = str.substring(0, str.indexOf(Constants.SPLIT_DIVIDE)).split(Constants.SPLIT_COMMA);
-        }
-        parameterObj.put("address", addressList);
-        parameterObj.put("userName", dataSource.getJdbcUsername() == null ? Constants.STRING_BLANK : dataSource.getJdbcUsername());
-        parameterObj.put("userPassword", dataSource.getJdbcPassword() == null ? Constants.STRING_BLANK : dataSource.getJdbcPassword());
-        parameterObj.put("dbName", dataSource.getDatabaseName());
+		parameterObj.put("url", dataSource.getJdbcUrl()+""+dataSource.getDatabaseName());
+		parameterObj.put("database",dataSource.getDatabaseName());
         parameterObj.put("collectionName", plugin.getReaderTable());
         parameterObj.put("column", plugin.getColumns());
         readerObj.put("parameter", parameterObj);
