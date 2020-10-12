4b53abb9d619abd6470a8c3206c3955242da605d
wxgzgl <wxgzgl@gmail.com>
2020-10-12 21:32:51 +0800

    update detail

--- flinkx-rpc/src/main/java/com/guoliang/flinkx/rpc/util/IpUtil.java
+++ flinkx-rpc/src/main/java/com/guoliang/flinkx/rpc/util/IpUtil.java
@@ -12,10 +12,11 @@ import java.util.Enumeration;
 import java.util.regex.Pattern;
 
 /**
- * ip tool
  *
- * @author xuxueli 2016-5-22 11:38:05
- */
+ * @Author: LarkMidTable
+ * @Date: 2020/9/16 11:14
+ * @Description: IP的工具类
+ **/
 public class IpUtil {
     private static final Logger logger = LoggerFactory.getLogger(IpUtil.class);
 
@@ -26,7 +27,6 @@ public class IpUtil {
 
     private static volatile InetAddress LOCAL_ADDRESS = null;
 
-    // ---------------------- valid ----------------------
 
     private static InetAddress toValidAddress(InetAddress address) {
         if (address instanceof Inet6Address) {
@@ -41,17 +41,20 @@ public class IpUtil {
         return null;
     }
 
-    private static boolean isPreferIPV6Address() {
+	/**
+	 *
+	 * @return
+	 */
+	private static boolean isPreferIPV6Address() {
         return Boolean.getBoolean("java.net.preferIPv6Addresses");
     }
 
-    /**
-     * valid Inet4Address
-     *
-     * @param address
-     * @return
-     */
-    private static boolean isValidV4Address(InetAddress address) {
+	/**
+	 *
+	 * @param address
+	 * @return 判断是不是有效的IPV4的地址
+	 */
+	private static boolean isValidV4Address(InetAddress address) {
         if (address == null || address.isLoopbackAddress()) {
             return false;
         }
@@ -62,39 +65,29 @@ public class IpUtil {
                 && !LOCALHOST_VALUE.equals(name));
     }
 
-
-    /**
-     * normalize the ipv6 Address, convert scope name to scope id.
-     * e.g.
-     * convert
-     * fe80:0:0:0:894:aeec:f37d:23e1%en0
-     * to
-     * fe80:0:0:0:894:aeec:f37d:23e1%5
-     * <p>
-     * The %5 after ipv6 address is called scope id.
-     * see java doc of {@link Inet6Address} for more details.
-     *
-     * @param address the input address
-     * @return the normalized address, with scope id converted to int
-     */
-    private static InetAddress normalizeV6Address(Inet6Address address) {
+	/**
+	 *
+	 * @param address
+	 * @return 返回IPV6的地址
+	 */
+	private static InetAddress normalizeV6Address(Inet6Address address) {
         String addr = address.getHostAddress();
         int i = addr.lastIndexOf('%');
         if (i > 0) {
             try {
                 return InetAddress.getByName(addr.substring(0, i) + '%' + address.getScopeId());
             } catch (UnknownHostException e) {
-                // ignore
                 logger.debug("Unknown IPV6 address: ", e);
             }
         }
         return address;
     }
 
-    // ---------------------- find ip ----------------------
-
-
-    private static InetAddress getLocalAddress0() {
+	/**
+	 *
+	 * @return 返回本地的IP
+	 */
+	private static InetAddress getLocalAddress0() {
         InetAddress localAddress = null;
         try {
             localAddress = InetAddress.getLocalHost();
@@ -145,13 +138,10 @@ public class IpUtil {
     }
 
 
-    // ---------------------- tool ----------------------
-
-    /**
-     * Find first valid IP from local network card
-     *
-     * @return first valid local IP
-     */
+	/**
+	 *
+	 * @return 返回本地的IP
+	 */
     public static InetAddress getLocalAddress() {
         if (LOCAL_ADDRESS != null) {
             return LOCAL_ADDRESS;
@@ -162,25 +152,29 @@ public class IpUtil {
     }
 
     /**
-     * get ip address
-     *
-     * @return String
+	 *
+     * @return 返回本地的IP
      */
     public static String getIp() {
         return getLocalAddress().getHostAddress();
     }
 
     /**
-     * get ip:port
      *
      * @param port
-     * @return String
+     * @return 返回本地IP:port
      */
     public static String getIpPort(int port) {
         String ip = getIp();
         return getIpPort(ip, port);
     }
 
+	/**
+	 *
+	 * @param ip
+	 * @param port
+	 * @return ip:port
+	 */
     public static String getIpPort(String ip, int port) {
         if (ip == null) {
             return null;
@@ -188,7 +182,11 @@ public class IpUtil {
         return ip.concat(":").concat(String.valueOf(port));
     }
 
-    public static Object[] parseIpPort(String address) {
+	/**
+     * @param address
+	 * @return ip和port的数组
+	 */
+	public static Object[] parseIpPort(String address) {
         String[] array = address.split(":");
 
         String host = array[0];
@@ -196,6 +194,4 @@ public class IpUtil {
 
         return new Object[]{host, port};
     }
-
-
 }
