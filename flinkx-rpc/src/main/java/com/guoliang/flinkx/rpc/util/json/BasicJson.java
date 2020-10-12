7f164bdca8599a08f06f3ce2b43bf047cc0c987e
wxgzgl <wxgzgl@gmail.com>
2020-10-12 22:09:58 +0800

    update detail

--- flinkx-rpc/src/main/java/com/guoliang/flinkx/rpc/util/json/BasicJson.java
+++ flinkx-rpc/src/main/java/com/guoliang/flinkx/rpc/util/json/BasicJson.java
@@ -6,60 +6,63 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author xuxueli 2018-11-30
- */
+ *
+ * @Author: LarkMidTable
+ * @Date: 2020/9/16 11:14
+ * @Description: Json的父类
+ **/
 public class BasicJson {
 
-    private static final BasicJsonReader basicJsonReader = new BasicJsonReader();
-    private static final BasicJsonwriter basicJsonwriter = new BasicJsonwriter();
+	private static final BasicJsonReader basicJsonReader = new BasicJsonReader();
+	private static final BasicJsonwriter basicJsonwriter = new BasicJsonwriter();
 
-    /**
-     * object to json
-     *
-     * @param object
-     * @return
-     */
-    public static String toJson(Object object) {
-        return basicJsonwriter.toJson(object);
-    }
+	/**
+	 * object to json
+	 *
+	 * @param object
+	 * @return
+	 */
+	public static String toJson(Object object) {
+		return basicJsonwriter.toJson(object);
+	}
 
-    /**
-     * parse json to map
-     *
-     * @param json
-     * @return only for filed type "null、ArrayList、LinkedHashMap、String、Long、Double、..."
-     */
-    public static Map<String, Object> parseMap(String json) {
-        return basicJsonReader.parseMap(json);
-    }
+	/**
+	 * parse json to map
+	 *
+	 * @param json
+	 * @return only for filed type "null、ArrayList、LinkedHashMap、String、Long、Double、..."
+	 */
+	public static Map<String, Object> parseMap(String json) {
+		return basicJsonReader.parseMap(json);
+	}
 
-    /**
-     * json to List
-     *
-     * @param json
-     * @return
-     */
-    public static List<Object> parseList(String json) {
-        return basicJsonReader.parseList(json);
-    }
+	/**
+	 * json to List
+	 *
+	 * @param json
+	 * @return
+	 */
+	public static List<Object> parseList(String json) {
+		return basicJsonReader.parseList(json);
+	}
 
 
-    public static void main(String[] args) {
-        Map<String, Object> result = new HashMap<>();
-        result.put("code", 200);
-        result.put("msg", "success");
-        result.put("arr", Arrays.asList("111", "222"));
-        result.put("float", 1.11f);
-        result.put("temp", null);
+	public static void main(String[] args) {
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("msg", "success");
+		result.put("arr", Arrays.asList("111", "222"));
+		result.put("float", 1.11f);
+		result.put("temp", null);
 
-        String json = toJson(result);
-        System.out.println(json);
+		String json = toJson(result);
+		System.out.println(json);
 
-        Map<String, Object> mapObj = parseMap(json);
-        System.out.println(mapObj);
+		Map<String, Object> mapObj = parseMap(json);
+		System.out.println(mapObj);
 
-        List<Object> listInt = parseList("[111,222,33]");
-        System.out.println(listInt);
+		List<Object> listInt = parseList("[111,222,33]");
+		System.out.println(listInt);
 
-    }
+	}
 }
