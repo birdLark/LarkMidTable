11c768a2646703a0321c0bc6f34060f9f956d401
wxgzgl <wxgzgl@gmail.com>
2021-01-04 20:10:50 +0800

    整理Engine类

--- flinkx-admin/src/main/java/com/guoliang/flinkx/admin/Engine.java
+++ flinkx-admin/src/main/java/com/guoliang/flinkx/admin/Engine.java
@@ -1,6 +1,7 @@
 package com.guoliang.flinkx.admin;
 
 import com.guoliang.flinkx.admin.entity.Common;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +15,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
- * Engine是flinkx-web入口类，该类负责数据的初始化
+ * Engine是入口类，该类负责数据的初始化
  */
 @EnableSwagger2
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
@@ -23,17 +24,22 @@ public class Engine {
     private static Logger logger = LoggerFactory.getLogger(Engine.class);
 
     public static void main(String[] args) throws UnknownHostException {
-        Environment env = new SpringApplication(Engine.class).run(args).getEnvironment();
+    	Environment env = new SpringApplication(Engine.class).run(args).getEnvironment();
+       //获取参数
         String port = env.getProperty(Common.SERVERPORT)== null ? Common.PORT : env.getProperty(Common.SERVERPORT);
-        String context = env.getProperty(Common.SERVERCONTEXTPATH)== null ? Common.PORT : env.getProperty(Common.SERVERCONTEXTPATH);
-        String docPath = Common.DOCPATH;
-        String path = port + "" + context + docPath;
-        String externalAPI = InetAddress.getLocalHost().getHostAddress();
-        logger.info(
+        String context = env.getProperty(Common.SERVERCONTEXTPATH)== null ? Common.CONTEXTPATH : env.getProperty(Common.SERVERCONTEXTPATH);
+		String hostAddress = InetAddress.getLocalHost().getHostAddress();
+		//拼凑路径
+		String localAPIPath = StringUtils.join(Common.PREFEX,Common.LOCALADDRESS,port,context,Common.DOCPATH);
+		String externalAPIPath = StringUtils.join(Common.PREFEX,hostAddress,port,context,Common.DOCPATH);
+		String webURLPath = StringUtils.join(Common.PREFEX,Common.LOCALADDRESS,port,context,Common.DOCPATH);
+		//打印日志
+		logger.info(
                 "Access URLs:\n----------------------------------------------------------\n\t"
-                        + "Local-API: \t\thttp://127.0.0.1:{}\n\t"
-                        + "External-API: \thttp://{}:{}\n\t"
-                        + "web-URL: \t\thttp://127.0.0.1:{}/index.html\n\t----------------------------------------------------------",
-                path, externalAPI, path, port);
+                        + "Local-API: \t\t{}\n\t"
+                        + "External-API: \t\t{}\n\t"
+                        + "web-URL: \t\thttp://127.0.0.1:{}/index.html\n\t"
+						+ "----------------------------------------------------------",
+				localAPIPath, externalAPIPath, webURLPath);
     }
 }
--- flinkx-admin/src/main/java/com/guoliang/flinkx/admin/entity/Common.java
+++ flinkx-admin/src/main/java/com/guoliang/flinkx/admin/entity/Common.java
@@ -7,8 +7,11 @@ package com.guoliang.flinkx.admin.entity;
  * @Description: 常量描述类
  **/
 public class Common {
+	public static String PREFEX ="http://";
+	public static String LOCALADDRESS ="127.0.0.1";
 	public static String PORT ="8080";
 	public static String SERVERPORT ="server.port";
+	public static String CONTEXTPATH ="";
 	public static String SERVERCONTEXTPATH ="server.contextPath";
 	public static String DOCPATH="/doc.html";
 }
