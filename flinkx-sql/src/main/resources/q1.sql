2ce45505af0f07593b23cd6c18263a7920798398
wxgzgl <wxgzgl@gmail.com>
2021-10-03 20:55:40 +0800

    sql语句的修改

--- flinkx-sql/src/main/resources/q1.sql
+++ flinkx-sql/src/main/resources/q1.sql
@@ -24,9 +24,9 @@ CREATE TABLE user_log (
     'connector.topic' = 'user_behavior',
     'connector.startup-mode' = 'earliest-offset',
     'connector.properties.0.key' = 'zookeeper.connect',
-    'connector.properties.0.value' = 'localhost:2181',
+    'connector.properties.0.value' = '192.168.1.204:2181',
     'connector.properties.1.key' = 'bootstrap.servers',
-    'connector.properties.1.value' = 'localhost:9092',
+    'connector.properties.1.value' = '192.168.1.204:9092',
     'update-mode' = 'append',
     'format.type' = 'json',
     'format.derive-schema' = 'true'
