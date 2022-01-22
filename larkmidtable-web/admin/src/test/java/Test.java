4b095d00af84949119d74af4325aff869d6cc5b8
wxgzgl <wxgzgl@gmail.com>
2022-01-22 17:05:00 +0800

    mysql到mysql的同步测试完成

--- larkmidtable-web/admin/src/test/java/Test.java
+++ larkmidtable-web/admin/src/test/java/Test.java
@@ -1,5 +1,7 @@
 import com.larkmidtable.admin.core.cron.CronExpression;
+import com.larkmidtable.core.util.ProcessUtil;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
 
@@ -11,11 +13,14 @@ import java.util.Date;
  **/
 public class Test {
 
-	public static void main(String[] args) throws ParseException {
-		CronExpression cronExpression = new CronExpression("00 * * * * ? *");
-		Date lastTime = new Date();
-		System.out.println(lastTime);
-		lastTime = cronExpression.getNextValidTimeAfter(lastTime);
-		System.out.println(lastTime);
+	public static void main(String[] args) throws ParseException, IOException {
+//		CronExpression cronExpression = new CronExpression("00 * * * * ? *");
+//		Date lastTime = new Date();
+//		System.out.println(lastTime);
+//		lastTime = cronExpression.getNextValidTimeAfter(lastTime);
+//		System.out.println(lastTime);
+		final Process process = Runtime.getRuntime().exec("python C:\\Users\\guoliang\\Desktop\\flinkx\\flinkx.py");
+		String prcsId = ProcessUtil.getProcessId(process);
+		System.out.println(prcsId);
 	}
 }
