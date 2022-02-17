556f20d293f16c2f5ca88a2532eaf20aced42e1b
wxgzgl <wxgzgl@gmail.com>
2022-02-17 10:00:04 +0800

    修改目录

---
+++ larkmidtable-bigdata/example/src/main/java/FlinkSocketWindowWordCount.java
@@ -0,0 +1,41 @@
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+
+public class FlinkSocketWindowWordCount {
+    public static void main(String[] args) throws Exception {
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		DataStream<Tuple2<String, Integer>> dataStream = env
+				.socketTextStream("192.168.1.204", 9000)
+				.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+
+					@Override
+					public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+
+						for (String word: sentence.split(" ")) {
+							out.collect(new Tuple2<String, Integer>(word, 1));
+						}
+					}
+				})
+				.keyBy(0)
+				.timeWindow(Time.days(10),Time.seconds(5))
+				.sum(1);
+
+		dataStream.print();
+
+		env.execute("Window WordCount");
+    }
+
+	public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+		@Override
+		public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+			for (String word: sentence.split(" ")) {
+				out.collect(new Tuple2<String, Integer>(word, 1));
+			}
+		}
+	}
+}
